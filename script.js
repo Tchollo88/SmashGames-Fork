2ff404e10bd731976963e410a682b2481e2ecf3e
Tchollo88 <Tcholloman@Gmail.com>
2023-05-25 11:53:54 -0700

    Blueprinted card format from index.html to script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const data = {
+    cards: [
+        { 
+            imgPath: "./pathsomewhere.img",
+            imgAltText: "some alternate text",
+            title: "The Title",
+            body: "some kind of body of text",
+            actionLink: "https://localhost:5500/games/game.html"
+        },
+        { 
+            imgPath: "./pathsomewhere.img",
+            imgAltText: "some alternate text",
+            title: "The Title",
+            body: "some kind of body of text",
+            actionLink: "https://localhost:5500/games/game.html"
+        },
+        { 
+            imgPath: "./pathsomewhere.img",
+            imgAltText: "some alternate text",
+            title: "The Title",
+            body: "some kind of body of text",
+            actionLink: "https://localhost:5500/games/game.html"
+        },
+    ]
+}
