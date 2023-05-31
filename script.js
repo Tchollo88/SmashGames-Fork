d077d61df32eac28b333f90a0919c282d429f256
Tchollo88 <Tcholloman@Gmail.com>
2023-05-31 11:50:10 -0700

    Reformatted changes for card layout in script.

--- script.js
+++ script.js
@@ -24,11 +24,47 @@ const Data = {
                 {
                     name: "blast",
                     pageName: "Inferno Blast",
-                    callToAction: {
-                        src:"images/Inferno-Jumbotron.png",
-                        altText: "Inferno Blast Gameplay",
-                        buttonLink: "https://steampowered.com"
-                    }
+                    blocks: [
+                        {
+                            type: "call-To-Action", 
+                            src:"images/Inferno-Jumbotron.png",
+                            altText: "Inferno Blast Gameplay",
+                            buttonLink: "https://steampowered.com",
+                            buttonLinkText: "Buy now on Steam"
+                            
+                        },
+                        {
+                            type: "description",
+                            text: "Description: Inferno Blast is an action-packed adventure game that takes players to a fiery realm of danger and excitement. In this game, you'll take on the role of a hero who must navigate through a series of treacherous levels filled with fiery obstacles and hordes of demons. With each level you complete, you'll unlock new weapons and power-ups to help you in your quest to defeat the ultimate demon boss. Can you survive the inferno and emerge victorious?",
+                        },
+                        {
+                            type: "deck",
+                            cards: [
+                                {
+                                    type: "card",
+                                    src: "images/Inferno/Inferno-2-short.png",
+                                    altText: "Inferno Blast Gameplay",
+                                    title: "Dynamic Environments",
+                                    description: "Navigate through a variety of fiery landscapes, including lava pits, crumbling ruins, and demonic strongholds."
+                                },
+                                {
+                                    type: "card",
+                                    src: "images/Inferno/Inferno-1-short.png",
+                                    altText: "Inferno Blast Gameplay", 
+                                    title: "Upgradable Weaponss",
+                                    description: "Customize your arsenal with a range of weapons and power-ups, including flamethrowers, rocket launchers, and more."
+                                },
+                                {
+                                    type: "card",
+                                    src: "images/Inferno/Inferno-3-short.png",
+                                    altText: "Inferno Blast Gameplay",
+                                    title: "Epic Boss Battle",
+                                    description: "Face off against towering demon bosses, each with their own unique strengths and weaknesses." 
+                                },
+                            ],
+                        }
+                    ],
+                    
                 },
                 {
                     name: "game2",
@@ -45,6 +81,4 @@ const Data = {
             pageName: "Contact Us"
         },
     ]
-
-
 }
